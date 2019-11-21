02e868f487535a4065db7aec9d29adab9e71ece4
tinmarino <tinmarino@gmail.com>
2019-11-20 21:41:40 -0300

    make change name m -> my beacuase mcopy is un /usr/bin

--- Martin/make_html.sh
+++ Martin/make_html.sh
@@ -1,9 +1,9 @@
 
-mcompile() {
+mycompile() {
     htxelatex martin_tourneboeuf_cv_english.tex "to_html.cfg,html,css-in,charset=utf-8" " -cunihtf -utf8"
 }
 
-mconfigure() {
+myconfigure() {
     perl -0777 -pe ' s/\<!DOCTYPE.*?\>//s' -i.bak martin_tourneboeuf_cv_english.html
     # Fix de merde
     perl -0777 -pe ' s:\s*(\</?(td|ul|li)):$1:gs' -i.bak martin_tourneboeuf_cv_english.html
@@ -13,6 +13,6 @@ mconfigure() {
         | pandoc --no-highlight --standalone --self-contained -o martin_tourneboeuf_cv_english.html
 }
 
-mmove(){
+mycopy(){
     cp martin_tourneboeuf_cv_english.html ~/Software/Html/Page/cv/
 }
