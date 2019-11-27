fa7e627b4439978c5942517ce8ba9e9c8e321092
tinmarino <tinmarino@gmail.com>
2019-11-27 16:17:42 -0300

    Martin update contact

--- Martin/make_html.sh
+++ Martin/make_html.sh
@@ -1,18 +1,26 @@
+myfiles="martin_tourneboeuf_cv_english martin_tourneboeuf_cv_french martin_tourneboeuf_cv_spanish"
 
 mycompile() {
-    htxelatex martin_tourneboeuf_cv_english.tex "to_html.cfg,html,css-in,charset=utf-8" " -cunihtf -utf8"
+    for f in $myfiles; do
+        htxelatex $f.tex "to_html.cfg,html,css-in,charset=utf-8" " -cunihtf -utf8"
+    done
 }
 
 myconfigure() {
-    perl -0777 -pe ' s/\<!DOCTYPE.*?\>//s' -i.bak martin_tourneboeuf_cv_english.html
-    # Fix de merde
-    perl -0777 -pe ' s:\s*(\</?(td|ul|li)):$1:gs' -i.bak martin_tourneboeuf_cv_english.html
-    cp martin_tourneboeuf_cv_english.html martin_tourneboeuf_cv_english_1.html
+    for f in $myfiles; do
 
-    cat martin_tourneboeuf_cv_english.html \
-        | pandoc --no-highlight --standalone --self-contained -o martin_tourneboeuf_cv_english.html
+        perl -0777 -pe ' s/\<!DOCTYPE.*?\>//s' -i.bak $f.html
+        # Fix de merde
+        perl -0777 -pe ' s:\s*(\</?(td|ul|li)):$1:gs' -i.bak $f.html
+        cp $f.html $f.html
+
+        cat $f.html \
+            | pandoc --no-highlight --standalone --self-contained -o $f.html
+    done
 }
 
 mycopy(){
-    cp martin_tourneboeuf_cv_english.html ~/Software/Html/Page/cv/
+    for f in $myfiles; do
+        cp $f.html ~/Software/Html/Page/cv/
+    done
 }
